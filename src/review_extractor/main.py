cae93712783371ec855942ffcf35ec4c4ad4821e
Michael Lasso <lassomicha@hotmail.com>
2025-05-31 07:28:09 -0400

    WIP

--- src/review_extractor/main.py
+++ src/review_extractor/main.py
@@ -1,5 +1,6 @@
 from review_extractor import orchestrator
 import logging
+import json
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -7,7 +8,9 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     logger.info(f"Received event: {event}")
-    message = orchestrator.start(event['text'])
+    body = json.loads(event['body'])
+    text = body.get("text")
+    message = orchestrator.start(text)
     logger.info(f"Processed message: {message}")
     return {
         "statusCode": 200,
